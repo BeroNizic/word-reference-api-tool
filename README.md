2fde5589857998b24ce176b28bd98c5e3fe1aac1
bero <bnizic@gmail.com>
2021-03-14 22:59:18 +0100

    initial working version

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # word-reference-api-tool
+
+This is a simple tool which, for the given list of words, calls Word reference API and create/append json file with the information retrieved from the [http://wordreference.com/](http://wordreference.com/).
+
+This tool uses [https://github.com/fega/wordreference-api](https://github.com/fega/wordreference-api)  
+
+Usage:
+
+1. npm install
+
+2. node **wr-api-tool** *word-list-file json-result-file from-lang to-lang*
+
+   Example:
+
+   node **wr-api-tool** *c:\test\words_to_translate.txt* *c:\test\wordReferenceInfo.json* *it* *en*
+
+Notes:
+
+1. If the *json-result-file* already exists, new translations will be appended to that file if a word which we want to translate doesn't yet exist in *json-result-file*
+2. in *word-list-file* the rule is **one word per line**
+3. Possible language values for *from-lang* and *to-lang* are: it, en, es, de, fr, pt (+ some other which you can check directly on the wordreference site)
---
+++ wr-api-tool.js
@@ -0,0 +1,98 @@
+var wr = require("wordreference-api");
+var fs = require("fs");
+
+let existingWords = [];
+let i = 0;
+let wordListFile = "";
+let fromLang = "it";
+let toLang = "en";
+let jsonResultFile = "";
+
+main();
+
+function main() {
+  if (!areValidParms()) return;
+
+  jsonResultFile = process.argv[3];
+  fromLang = process.argv[4];
+  toLang = process.argv[5];
+  wordListFile = process.argv[2];
+
+  if (fs.existsSync(jsonResultFile)) {
+    fs.readFile(jsonResultFile, "utf8", function (err, data) {
+      existingWords = JSON.parse(data);
+      callWRAPiForWords();
+    });
+  } else callWRAPiForWords();
+}
+
+function areValidParms() {
+  if (process.argv.length != 6) {
+    console.error("Usage: call-word-reference word-list-file json-result-file from-lang to-lang");
+    console.log(
+      "word-list-file: text file with the list of words. One word per line. For every word will be called word reference API if that word isn't already present in the json-result-file."
+    );
+    console.log("json-result-file: .json file with the result of word reference API call for every word.");
+    console.log("from-lang: original language. For example 'it'");
+    console.log("to-lang: in which language to translate. For example 'en'");
+    return false;
+  }
+
+  let wordListFile = process.argv[2];
+  if (!fs.existsSync(wordListFile)) {
+    console.error(`${wordListFile} doesn't exist`);
+    return false;
+  }
+  return true;
+}
+
+function callWRAPiForWords() {
+  fs.readFile(wordListFile, "utf8", function (err, data) {
+    var lines = data.split("\r\n");
+    lines.forEach((line) => processLine(lines, line));
+  });
+}
+
+function processLine(lines, line) {
+  if (existingWords.find((elem) => elem.initialWord === line || elem.word === line)) {
+    console.log(line + " exists");
+    i++;
+    writeToJsonFileIfNeeded(lines);
+    return;
+  }
+
+  wr(line, fromLang, toLang).then(
+    (result) => {
+      result.initialWord = line;
+      console.log(result);
+      existingWords.push(result);
+      i++;
+      writeToJsonFileIfNeeded(lines);
+    },
+    (error) => {
+      console.log(error);
+      i++;
+      writeToJsonFileIfNeeded(lines);
+    }
+  );
+}
+
+function readWordFromWR(word) {
+  wr(word, fromLang, toLang).then(
+    (result) => {
+      console.log(JSON.stringify(result, null, 2));
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+}
+
+function writeToJsonFileIfNeeded(lines) {
+  if (i < lines.length) return;
+  var json = JSON.stringify(existingWords, null, 2);
+  fs.writeFile(jsonResultFile, json, function (err) {
+    if (err) throw err;
+    console.log("Successfully written.");
+  });
+}
